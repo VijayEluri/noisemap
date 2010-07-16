4a650aec747d00f7050ebabab5d65a9fba7f93af
Nikos Maris <nickmeet@gmail.com>
2010-07-16 18:14:04 +0300

    test push

---
+++ java/eclipseworkspace/delete/src/maris/learning/Printing.java
@@ -0,0 +1,13 @@
+package maris.learning;
+
+public class Printing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test.");
+	}
+
+}
