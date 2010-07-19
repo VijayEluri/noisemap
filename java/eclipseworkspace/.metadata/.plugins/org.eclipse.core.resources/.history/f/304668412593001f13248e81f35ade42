7def9bdbaab9d08b7da36ecaebb20a102e625511
Nikos Maris <nickmeet@gmail.com>
2010-07-19 18:03:56 +0300

    .

---
+++ java/eclipseworkspace/.metadata/.plugins/org.eclipse.core.resources/.history/f/304668412593001f13248e81f35ade42
@@ -0,0 +1,14 @@
+package maris.learning.unittesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void testPerson() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ java/eclipseworkspace/unittesting/src/test/PersonTest.java
@@ -0,0 +1,18 @@
+package test;
+
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+//import maris.learning.unittesting.*;
+
+public class PersonTest {
+//	Person p = new Person();
+//	@Test
+//	public void testDefaultName() {
+//		assertEquals("unknown name",p.getName());
+//	}
+//	@Test
+//	public void testDefaultMax(){
+//		assertEquals(3,p.getMaxbooks());
+//	}
+
+}
